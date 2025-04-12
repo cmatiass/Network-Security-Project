28b6955601b695d9b23f2d99e5f79423b6cf714c
cmatias <cmatias@cofares.es>
2025-04-12 14:41:18 +0200

    Changes in data transformation

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -1,26 +1,26 @@
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
-## Configuration of the Data Ingestion
+
+## configuration of the Data Ingestion Config
 
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
-
 import os
 import sys
-import pandas as pd
 import numpy as np
+import pandas as pd
 import pymongo
 from typing import List
-from sklearn.model_selection import train_test_split    
-
+from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 
+
 class DataIngestion:
-    def __init__(self, data_ingestion_config:DataIngestionConfig):
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
             self.data_ingestion_config=data_ingestion_config
         except Exception as e:
@@ -28,7 +28,7 @@ class DataIngestion:
         
     def export_collection_as_dataframe(self):
         """
-        Read data from MongoDB and return as DataFrame
+        Read data from mongodb
         """
         try:
             database_name=self.data_ingestion_config.database_name
@@ -39,19 +39,21 @@ class DataIngestion:
             df=pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
                 df=df.drop(columns=["_id"],axis=1)
-            df.replace({"na": np.nan},inplace=True)
+            
+            df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
+            raise NetworkSecurityException
         
-    def export_data_into_feature_store(self, dataframe:pd.DataFrame):
+    def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
-            dir_path=os.path.dirname(feature_store_file_path)
+            #creating folder
+            dir_path = os.path.dirname(feature_store_file_path)
             os.makedirs(dir_path,exist_ok=True)
             dataframe.to_csv(feature_store_file_path,index=False,header=True)
             return dataframe
-
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -84,7 +86,8 @@ class DataIngestion:
             
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
+        
+        
     def initiate_data_ingestion(self):
         try:
             dataframe=self.export_collection_as_dataframe()
@@ -95,4 +98,4 @@ class DataIngestion:
             return dataingestionartifact
 
         except Exception as e:
-            raise NetworkSecurityException(e,sys)
+            raise NetworkSecurityException
--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://cmatiass:<@password>@cluster0.fvb1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://cmatiass:Admin123@cluster0.fvb1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
